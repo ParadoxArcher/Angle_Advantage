2de7cfe583d868a6db510e4f4dde2982b28b4519
ParadoxArcher <167530800+ParadoxArcher@users.noreply.github.com>
2025-01-14 09:26:36 -0800

    vault backup: 2025-01-14 09:26:36

--- zProject Files/Godot/Scripts/PlayerScript.gd
+++ zProject Files/Godot/Scripts/PlayerScript.gd
@@ -10,7 +10,7 @@ var isBraking = false
 
 ## Boost Variables
 @export var MaxSpeed = [1000, 1000] # {0: Fluctuating, 1: BaseMaxSpeed} ## Beware DodgeMaxSpeed
-@export var SpeedAccel = .015
+@export var SpeedAccel = .025
 @export var SpeedDecel = [.01, .01] # {0: Fluctuating,  1: Decel} ## Beware BrakeDecelMult
 @export var BoostDecay = [0, .3, .005] # {0: Fluctuating,  1:BoostRelease, 2: DecayRate}
 var BoostDir = Vector2(0, 0)
@@ -54,11 +54,10 @@ func _physics_process(_delta):
 	if MoveInput.y > 0  or BoostDecay[0] > 0:
 		BoostDir = Vector2(cos(rotation), sin(rotation))
 		var CounterAccel = -BoostDir.dot(velocity / MaxSpeed[1]) * CounterScaler[0]
-		AccelRate = SpeedAccel + (SpeedDecel[0] * CounterAccel ) + SpeedDecel[1] 
+		AccelRate = SpeedAccel + (clampf(SpeedDecel[0], 0, SpeedAccel) * CounterAccel )
 		print(AccelRate)
-		
-		if absf(AccelRate) < SpeedDecel[1]: # Debugging for AccelRate bandaid fix
-			push_warning("AccelRate is Clamped")
+		if SpeedDecel[0] >= SpeedAccel: # Debugging for AccelRate bandaid fix
+			push_warning("AccelRate is Clamped, " + str(AccelRate))
 			
 		if MoveInput.y == 0: # Boost Decay
 			BoostDir *= (BoostDecay[1] * BoostDecay[0])
