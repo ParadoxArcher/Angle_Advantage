6497328b67bdace060970b0ce9785a0666069306
ParadoxArcher <167530800+ParadoxArcher@users.noreply.github.com>
2025-01-16 08:52:40 -0800

    vault backup: 2025-01-16 08:52:40

--- zProject Files/Godot/Scripts/PlayerScript.gd
+++ zProject Files/Godot/Scripts/PlayerScript.gd
@@ -77,7 +77,7 @@ func _physics_process(_delta):
 	velocity = lerp(velocity, velocity.normalized(), SpeedDecel[0]) # Momentum
 	velocity = lerp(velocity, BoostDir * MaxSpeed[0], SpeedAccel) # Acceleration
 	
-	move_and_slide()
+	set_safe_margin(1)
 	var collision = move_and_collide(velocity * _delta)
 	if collision:
 		velocity = velocity.bounce(collision.get_normal()) 
