2564e53804b76f8e166ad357f1a9990275309e0d
ParadoxArcher <167530800+ParadoxArcher@users.noreply.github.com>
2025-03-14 11:33:08 -0700

    vault backup: 2025-03-14 11:33:08

--- zProject Files/Godot/Scenes/BoostDetection.gd
+++ zProject Files/Godot/Scenes/BoostDetection.gd
@@ -1,10 +1,14 @@
 extends Area2D
 
-#Make array
+var Bodies
+
+func _ready():
+	Bodies = [get_parent()]
 
 func _on_body_shape_entered(body_rid, body, body_shape_index, local_shape_index):
-	if body != get_parent():
-		print(body.position)
+	if body != Bodies[0]:
+		Bodies.append(body)
+		print(Bodies)
 		# add body to array
 	
 func _physics_process(_delta):
@@ -14,8 +18,7 @@ func _physics_process(_delta):
 # get collision normal
 # return collision normal to PlayerScript via signal
 
-
-
-
 func _on_body_shape_exited(body_rid, body, body_shape_index, local_shape_index):
-	pass # Replace with function body.
+	if body != Bodies[0]:
+		print(body.position)
+		
