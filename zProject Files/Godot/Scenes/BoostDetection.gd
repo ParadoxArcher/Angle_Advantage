0427e62b011bc6f5caf04a6a3738ce4a964d5a6a
ParadoxArcher <167530800+ParadoxArcher@users.noreply.github.com>
2025-03-14 10:47:08 -0700

    vault backup: 2025-03-14 10:47:08

--- zProject Files/Godot/Scenes/BoostDetection.gd
+++ zProject Files/Godot/Scenes/BoostDetection.gd
@@ -1,9 +1,17 @@
 extends Area2D
 
-func _physics_process(_delta):
-	print(Collision.get_normal())
 
-
-
-func _on_body_entered(body):
+## detect collision (on body shape entered)
+# get body shape
+func _on_body_shape_entered(body_rid, body, body_shape_index, local_shape_index):
+	print(body.shape_find_owner(body_shape_index))
 	pass # Replace with function body.
+	
+func _physics_process(_delta):
+	pass
+## in process
+# create raycast towards body shape
+# get collision normal
+# return collision normal to PlayerScript via signal
+
+## end collision (body shape exited)
