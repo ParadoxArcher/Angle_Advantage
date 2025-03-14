a284b788ee92a00b5bf02d4a6c3880465600f6ee
ParadoxArcher <167530800+ParadoxArcher@users.noreply.github.com>
2025-03-14 10:00:08 -0700

    vault backup: 2025-03-14 10:00:08

--- zProject Files/Godot/Scenes/BoostDetection.gd
+++ zProject Files/Godot/Scenes/BoostDetection.gd
@@ -1,17 +1,13 @@
 extends Area2D
 
-@export var WallBoostScale = .25 # {0: Fluctuating, 1: Multiplier}
 
-@onready var ray_cast_l = $RayCastL
-@onready var ray_cast_ml = $RayCastML
-@onready var ray_cast_m = $RayCastM
-@onready var ray_cast_mr = $RayCastMR
-@onready var ray_cast_r = $RayCastR
-
-signal WallBoosting(TotalBoost)
-
-on
 
 
 func _on_body_entered(body):
-	pass # Replace with function body.
+	
+	print("in yo walls")
+	# detect wall
+	# get collision normal
+	# return collision normal
+	
+	pass
