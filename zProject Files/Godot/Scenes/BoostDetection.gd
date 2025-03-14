fcdecd86f81f6fb2260e3de2761aeebd586179ac
ParadoxArcher <167530800+ParadoxArcher@users.noreply.github.com>
2025-03-14 12:04:08 -0700

    vault backup: 2025-03-14 12:04:08

--- zProject Files/Godot/Scenes/BoostDetection.gd
+++ zProject Files/Godot/Scenes/BoostDetection.gd
@@ -1,18 +1,20 @@
 extends Area2D
 
-var Bodies
+@onready var Bodies = [get_parent()]
+@onready var raycast = $RayCast2D
 
-func _ready():
-	Bodies = [get_parent()]
 
 func _on_body_shape_entered(body_rid, body, body_shape_index, local_shape_index):
 	if body != Bodies[0]:
 		Bodies.append(body)
+		print([Bodies])
 	
 func _physics_process(_delta):
-	pass
-## in process
-# create raycast towards body shape
+	for Body in Bodies:
+		pass
+	
+# per index
+#direct raycast towards body shape
 # get collision normal
 # return collision normal to PlayerScript via signal
 
