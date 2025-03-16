ca37b2fa8ff3e33b011caacd5178b1feb9435e76
ParadoxArcher <167530800+ParadoxArcher@users.noreply.github.com>
2025-03-16 02:15:14 -0700

    vault backup: 2025-03-16 02:15:14

--- zProject Files/Godot/Scenes/BoostDetection.gd
+++ zProject Files/Godot/Scenes/BoostDetection.gd
@@ -8,6 +8,7 @@ var CollisionNormal
 func _on_body_shape_entered(body_rid, body, body_shape_index, local_shape_index):
 	if body != Bodies[0]:
 		Bodies.append(body)
+		print(body.get_extents())
 	
 func _physics_process(_delta):
 	
