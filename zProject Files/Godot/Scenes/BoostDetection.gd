408cb2c9584e6065fa321a5bcc60b75fd103a47e
ParadoxArcher <167530800+ParadoxArcher@users.noreply.github.com>
2025-03-19 15:08:52 -0700

    vault backup: 2025-03-19 15:08:52

--- zProject Files/Godot/Scenes/BoostDetection.gd
+++ zProject Files/Godot/Scenes/BoostDetection.gd
@@ -8,8 +8,8 @@ var CollisionNormal
 
 
 func _on_body_shape_entered(body_rid, body, body_shape_index, local_shape_index):
-	var BodyCollider = shape_find_owner(local_shape_index)
-	print(str(BodyCollider))
+	var BodyCollider = shape_find_owner(body_shape_index)
+	print(str(body_shape_index) + ", " + str(BodyCollider))
 	if body != Bodies[0]:
 		Bodies.append(body)
 	
