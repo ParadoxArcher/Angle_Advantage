f335240242753cb7413a7c583c8901671b75b52e
ParadoxArcher <167530800+ParadoxArcher@users.noreply.github.com>
2025-03-19 16:26:15 -0700

    vault backup: 2025-03-19 16:26:15

--- zProject Files/Godot/Scenes/BoostDetection.gd
+++ zProject Files/Godot/Scenes/BoostDetection.gd
@@ -9,7 +9,9 @@ var CollisionNormal
 
 func _on_body_shape_entered(body_rid, body, body_shape_index, local_shape_index):
 	print(str(body_rid))
-	print(str(PhysicsServer2D.body_get_shape(body_rid, 0)))
+	var shapeRID = PhysicsServer2D.body_get_shape(body_rid, 0)
+	print(str(shapeRID))
+	print(str(PhysicsServer2D.shape_get_data(shapeRID)))
 	if body != Bodies[0]:
 		Bodies.append(body)
 	
