79ee5bc7113de2acf76b0056a233f87f33d252ae
ParadoxArcher <167530800+ParadoxArcher@users.noreply.github.com>
2025-03-19 23:59:14 -0700

    vault backup: 2025-03-19 23:59:14

--- zProject Files/Godot/Scenes/BoostDetection.gd
+++ zProject Files/Godot/Scenes/BoostDetection.gd
@@ -1,39 +1,36 @@
 extends Area2D
 
-@onready var Bodies = [get_parent()]
+@onready var ColliderInfo = {}
 @onready var raycast = $RayCast2D
 var CollisionNormal
 
 
 
 
-func _on_body_shape_entered(body_rid, body, body_shape_index, local_shape_index):
-	print("body: " + str(body) + " || body_rid: " + str(body_rid) + " || body_shape_index: " + str(body_shape_index))
-	var shapeRID = PhysicsServer2D.body_get_shape(body_rid, body_shape_index)
-	print("shape_RID: " + str(shapeRID))
-	print("shape_transform: " + str(PhysicsServer2D.body_get_shape_transform(body_rid, body_shape_index)) + " || shape_type: " + str(PhysicsServer2D.shape_get_type(shapeRID)) + " || shape_data: " + str(PhysicsServer2D.shape_get_data(shapeRID)))
-
-	if body != Bodies[0]:
-		Bodies.append(body)
+func _on_body_shape_entered(body_rid, body, body_shape_index, local_shape_index):	
+	
+	var shape_rid = PhysicsServer2D.body_get_shape(body_rid, body_shape_index)
+	#var local_pos = PhysicsServer2D.body_get_shape_transform(body_rid, body_shape_index).origin
+	print(str(PhysicsServer2D.body_get_shape_transform(body_rid, body_shape_index).origin))
+	#ColliderInfo[str(shape_rid)] = []
+	#print("body: " + str(body) + " || body_rid: " + str(body_rid) + " || body_shape_index: " + str(body_shape_index))
+	#print("shape_RID: " + str(shapeRID))
+	#print("shape_transform: " + str(PhysicsServer2D.body_get_shape_transform(body_rid, body_shape_index)) + " || shape_type: " + str(PhysicsServer2D.shape_get_type(shapeRID)) + " || shape_data: " + str(PhysicsServer2D.shape_get_data(shapeRID)))
 	
 func _physics_process(_delta):
 	
 	var ParentGlobalPos = get_parent().position
 	var ParentRotation = get_parent().rotation
 	
-	for Body in Bodies:
-		if Body != Bodies[0]:
-			var RelativeGlobalPos = Body.position - ParentGlobalPos
-			var LocalPos = Vector2(cos(ParentRotation) * RelativeGlobalPos.x, sin(ParentRotation) * RelativeGlobalPos.y)
-			raycast.target_position = LocalPos
-			if raycast.is_colliding():
-				CollisionNormal = raycast.get_collision_normal()
+	for Body in ColliderInfo:
+		var RelativeGlobalPos = Body.position - ParentGlobalPos
+		var LocalPos = Vector2(cos(ParentRotation) * RelativeGlobalPos.x, sin(ParentRotation) * RelativeGlobalPos.y)
+
 	
 #direct raycast towards body shape
 # get collision normal
 # return collision normal to PlayerScript via signal
 
 func _on_body_shape_exited(body_rid, body, body_shape_index, local_shape_index):
-	if body != Bodies[0]:
-		Bodies.erase(body)
+	ColliderInfo.erase(body_rid)
 	
