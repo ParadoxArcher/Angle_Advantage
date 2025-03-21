5d087d1a4cb75b542f414afa73ba1bebe21491b2
ParadoxArcher <167530800+ParadoxArcher@users.noreply.github.com>
2025-03-21 10:05:24 -0700

    vault backup: 2025-03-21 10:05:24

--- zProject Files/Godot/Scenes/Rainbowjpg.gd
+++ zProject Files/Godot/Scenes/Rainbowjpg.gd
@@ -1,16 +1,13 @@
 extends Sprite2D
 
-
+var x = 0
 # Called when the node enters the scene tree for the first time.
-func _ready():
-	print(transform)
-	
-	transform.y.y = -1
-	transform.x.x = -1
-	
-	print(transform)
-
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+	x += .02
+	var circle = cos(x)
+	
+	print(transform)
+
+	transform.x.x = circle
