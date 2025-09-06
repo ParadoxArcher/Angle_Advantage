d9f8aca97ac7b832b1e866ecf433fe1f3134cc33
ParadoxArcher <167530800+ParadoxArcher@users.noreply.github.com>
2025-09-06 04:42:12 -0700

    vault backup: 2025-09-06 04:42:12

--- zProject Files/Godot/Scripts/PlayerMovement.gd
+++ zProject Files/Godot/Scripts/PlayerMovement.gd
@@ -146,7 +146,8 @@ func _physics_process(_delta):
 		for _each in get_slide_collision_count():
 			var _CollisionLocal = to_local(get_slide_collision(_each).get_position())
 			
-			print((-_CollisionLocal).angle_to(velocity - _CollisionLocal))
+			print(pingpong((-_CollisionLocal ).normalized().angle_to(velocity.normalized() - _CollisionLocal.normalized()) + PI/2, PI) - PI/2)
+			print((-_CollisionLocal ).normalized().angle_to(velocity.normalized() - _CollisionLocal.normalized()))
 			var _InertiaAngle = get_wall_normal().rotated(sign(_RotationVelocity) * PI/4 * abs(_RotationVelocity / MaxRota ))
 			velocity += _InertiaAngle * _CollisionLocal.length() * abs(_RotationVelocity) * 10
 	#endregion
